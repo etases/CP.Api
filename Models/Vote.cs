63ab5fdd53dd7525bc6f3f8daf475d0ae027f86f
Huynh Tien <huynhqtienvtag@gmail.com>
2022-06-16 20:02:31 +0700

    add Vote model (#9)

---
+++ Models/Vote.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CP.Api.Models;
+
+public class Vote
+{
+    public int AccountId { get; set; }
+    public int CommentId { get; set; }
+    public bool IsUpvote { get; set; } // true = upvote, false = downvote
+
+    [ForeignKey(nameof(AccountId))]
+    [InverseProperty("Votes")]
+    public Account Account { get; set; } = null!;
+
+    [ForeignKey(nameof(CommentId))]
+    [InverseProperty("Votes")]
+    public Comment Comment { get; set; } = null!;
+}

aad4ab17641ba5c28f2263e87d665269f6ae61de
HSGamer <huynhqtienvtag@gmail.com>
2022-06-30 00:42:05 +0700

    add constant DefaultRoles for Authorize

--- Models/DefaultRoles.cs
+++ Models/DefaultRoles.cs
@@ -2,7 +2,10 @@
 
 public static class DefaultRoles
 {
-    public static readonly Role Administrator = new Role { Id = 1, Name = "Administrator" };
-    public static readonly Role User = new Role { Id = 2, Name = "User" };
-    public static readonly Role Manager = new Role { Id = 3, Name = "Manager" };
+    public const string AdministratorString = "Administrator";
+    public const string UserString = "User";
+    public const string ManagerString = "Manager";
+    public static readonly Role Administrator = new Role { Id = 1, Name = AdministratorString };
+    public static readonly Role User = new Role { Id = 2, Name = UserString };
+    public static readonly Role Manager = new Role { Id = 3, Name = ManagerString };
 }

db8128dc3a225921f35112f0e4cdd2a2a418ad44
TuLX <lexuantu1002@gmail.com>
2022-07-06 15:15:29 +0700

    add statistic service

---
+++ DTOs/Statistic/StatisticOutput.cs
@@ -0,0 +1,11 @@
+namespace CP.Api.DTOs.Statistic
+{
+    public class StatisticOutput
+    {
+        public DateTime? Date { get; set; } = DateTime.Today.Date;
+        public int? Day { get; set; }
+        public int? Month { get; set; }
+        public int? Year { get; set; }
+        public int? Total { get; set; }
+    }
+}
